7547e6d3cf7b0512f1ae22e5739c42a672fb1b07
Dimitris <dimitriskarageorgopoulos@gmail.com>
2020-06-08 15:55:37 +0300

    updates

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,24 @@ import React, { Component, Fragment } from "react";
 import Header from "./Layouts/Header";
 import Footer from "./Layouts/Footer";
 import Exercises from "./Exercises";
+import { muscles, exercises } from "../stor";
+
 export default class extends Component {
+  state = {
+    exercises
+  };
+
+  getExerciseByMuscle() {
+    return this.state.exercises;
+  }
+
   render() {
+    console.log(this.getExerciseByMuscle());
     return (
       <Fragment>
         <Header />
         <Exercises />
-        <Footer />
+        <Footer muscles={muscles} />
       </Fragment>
     );
   }
--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import { Paper } from "@material-ui/core";
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane";
 
 const styles = {
   Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
@@ -9,10 +10,10 @@ const styles = {
 export default props => (
   <Grid container>
     <Grid item sm>
-      <Paper style={styles.Paper}>text1</Paper>
+      <LeftPane styles={styles} />
     </Grid>
     <Grid item sm>
-      <Paper style={styles.Paper}>text2</Paper>
+      <RightPane styles={styles} />
     </Grid>
   </Grid>
 );
